de874365b3284ee551b2717d27125780f61f9cca
rkorva <rkorva@murraystate.edu>
2025-05-26 13:29:28 -0500

    Contact Model is been created

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
